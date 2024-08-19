3863ca05f380f4db332ba37052344c036483ccce
aqles <70847820+aqles@users.noreply.github.com>
2024-08-19 14:22:51 +0700

    Create alwayspop.js

---
+++ alwayspop.js
@@ -0,0 +1,9 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    // harusnya otomatis menjalankan fungsi untuk menampilkan popup hehe
+    var popup = document.querySelector('.poptamv');
+    if (popup) {
+      popup.style.display = 'block'; // bismmillah
+    }
+  });
+</script>
